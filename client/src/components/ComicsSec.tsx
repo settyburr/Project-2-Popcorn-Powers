616cdbeed76e04a34b18f5e759b9cf62732c9fe8
Buhzurk <achrismora@gmail.com>
2024-11-08 13:26:19 -0600

    made marvelcharacter interface into a file in interfaces

---
+++ client/src/components/ComicsSec.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import MarvelService from '../../../server/src/service/marvelService';
+import { MarvelCharacter } from '../interfaces/HeroData';
+
+const 
---
+++ client/src/pages/ComicsPage.tsx
@@ -0,0 +1,8 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+import { retrieveComics } from "../api/marvelAPI";
+import type { HeroData } from "../interfaces/HeroData";
+import ErrorPage from "./ErrorPage";
+
+
+
+
